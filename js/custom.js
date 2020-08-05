154afdc12bf4db63dcf52be9f10d3b0eb9b7ff72
Bakkar1 <68107222+Bakkar1@users.noreply.github.com>
2020-08-05 13:25:32 +0200

    ini01

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var mijnSpan = document.getElementById('mijnSpan'),
-    mijnLi = document.getElementById('mijnLi');
+    mijnLi = document.getElementById('mijnLi'),
+    mijnSection = document.getElementById('mijnSection');
 
 mijnSpan.onclick = function(){
     mijnLi.classList.toggle('mijn-bar');
+    mijnSection.classList.toggle('mijnS');
 }
