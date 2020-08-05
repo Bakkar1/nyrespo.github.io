b9e6ff6b03f5940b4d5e616708a69b5a73de79a0
Bakkar1 <68107222+Bakkar1@users.noreply.github.com>
2020-08-05 21:02:25 +0200

    hi

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,11 @@
 
 var mijnSpan = document.getElementById('mijnSpan'),
     mijnLi = document.getElementById('mijnLi'),
-    mijnSection = document.getElementById('mijnSection');
+    mijnSection = document.getElementById('mijnSection'),
+    mijnButton1 = document.getElementById('sButton1');
 
 mijnSpan.onclick = function(){
     mijnLi.classList.toggle('mijn-bar');
     mijnSection.classList.toggle('mijnS');
+    mijnButton1.setAttribute('disabled', 'disabled')
 }
